c72cc9b929e11f08f7d2268c1758d2d0061bd095
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2024-01-15 22:30:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+// Create an object called student with a property called name
+const student = {
+  name: "acciojob"
+};
+
+// Add a property to the Object prototype called getKeys() that returns an array of all the keys in the object
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Example usage
+const keys = student.getKeys();
+console.log(keys); // Output: ['name']
